8f2120d49832bc0f5fc93eea13a8ef37d707c765
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2021-11-22 06:05:25 +0900

    update viewPager(will adjust size)

--- app/src/main/java/com/example/mp2021_2_9/PromoteMainFrag.java
+++ app/src/main/java/com/example/mp2021_2_9/PromoteMainFrag.java
@@ -4,43 +4,74 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import me.relex.circleindicator.CircleIndicator3;
+
 public class PromoteMainFrag extends Fragment {
-    private ViewPager2 sliderViewPager;
-    private LinearLayout layoutIndicator;
-    private ViewGroup viewGroup;
+    private ViewGroup v;
+    private int page_num = 4;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.promotion_main_screen, container, false);
-
+        v = (ViewGroup) inflater.inflate(R.layout.promotion_main_screen, container, false);
         setInit();
-
-        return viewGroup;
+        return v;
     }
 
     private void setInit(){
-        sliderViewPager = viewGroup.findViewById(R.id.sliderViewPager);
-        FragmentStateAdapter SetupPagerAdapter = new FragmentStateAdapter(getActivity()) {
+        ViewPager2 viewPageSetUp = (ViewPager2) v.findViewById(R.id.viewpager);
+
+        ImageSliderAdapter setUpPagerAdapter = new ImageSliderAdapter(getActivity(), page_num);
+        viewPageSetUp.setAdapter(setUpPagerAdapter);
+
+        CircleIndicator3 indicator = (CircleIndicator3) v.findViewById(R.id.indicator);
+        indicator.setViewPager(viewPageSetUp);
+        indicator.createIndicators(page_num, 0);
+
+        viewPageSetUp.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        viewPageSetUp.setOffscreenPageLimit(page_num);
+        viewPageSetUp.setCurrentItem(1000);
+
+        viewPageSetUp.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public int getItemCount() {
-                return 0;
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if(positionOffsetPixels == 0){
+                    viewPageSetUp.setCurrentItem(position);
+                }
             }
 
-            @NonNull
             @Override
-            public Fragment createFragment(int position) {
-                return null;
+            public void onPageSelected(int position){
+                super.onPageSelected(position);
+                indicator.animatePageSelected(position%page_num);
             }
-        };
+        });
+
+        final float pageMargin = (float) getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+        final float pageOffset = (float) getResources().getDimensionPixelOffset(R.dimen.offset);
+        viewPageSetUp.setPageTransformer(new ViewPager2.PageTransformer(){
+            @Override
+            public void transformPage(@NonNull View page, float position){
+                float myOffset = (float) (position * -(2*pageOffset+pageMargin));
+                if(viewPageSetUp.getOrientation() == ViewPager2.ORIENTATION_HORIZONTAL){
+                    if(ViewCompat.getLayoutDirection(viewPageSetUp) == ViewCompat.LAYOUT_DIRECTION_RTL){
+                        page.setTranslationX(-myOffset);
+                    }else{
+                        page.setTranslationX(myOffset);
+                    }
+                }else{
+                    page.setTranslationY(myOffset);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mp2021_2_9/ViewPagerImages.java
@@ -0,0 +1,85 @@
+package com.example.mp2021_2_9;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ViewPagerImages {
+    public static class ViewPageImage1 extends Fragment{
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            ViewGroup v = (ViewGroup) inflater.inflate(R.layout.item_slider1, container, false);
+            ImageButton toPost = (ImageButton) v.findViewById(R.id.main_promo1);
+            toPost.setOnClickListener(new View.OnClickListener() {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                @Override
+                public void onClick(View v) {
+                    intent.setData(Uri.parse("https://www.instagram.com/p/CWjPkGDh_2D/?utm_medium=copy_link"));
+                    startActivity(intent);
+                }
+            });
+            return v;
+        }
+    }
+
+    public static class ViewPageImage2 extends Fragment{
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            ViewGroup v = (ViewGroup) inflater.inflate(R.layout.item_slider2, container, false);
+            ImageButton toPost = (ImageButton) v.findViewById(R.id.main_promo2);
+            toPost.setOnClickListener(new View.OnClickListener() {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                @Override
+                public void onClick(View v) {
+                    intent.setData(Uri.parse("https://www.instagram.com/p/CWjPmJnhHsj/?utm_medium=copy_link"));
+                    startActivity(intent);
+                }
+            });
+            return v;
+        }
+    }
+    public static class ViewPageImage3 extends Fragment{
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            ViewGroup v = (ViewGroup) inflater.inflate(R.layout.item_slider3, container, false);
+            ImageButton toPost = (ImageButton) v.findViewById(R.id.main_promo3);
+            toPost.setOnClickListener(new View.OnClickListener() {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                @Override
+                public void onClick(View v) {
+                    intent.setData(Uri.parse("https://www.instagram.com/p/CWjPqSoBD1r/?utm_medium=copy_link"));
+                    startActivity(intent);
+                }
+            });
+            return v;
+        }
+    }
+    public static class ViewPageImage4 extends Fragment{
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            ViewGroup v = (ViewGroup) inflater.inflate(R.layout.item_slider4, container, false);
+            ImageButton toPost = (ImageButton) v.findViewById(R.id.main_promo4);
+            toPost.setOnClickListener(new View.OnClickListener() {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                @Override
+                public void onClick(View v) {
+                    intent.setData(Uri.parse("https://www.instagram.com/p/CWjPs4ehHDS/?utm_medium=copy_link"));
+                    startActivity(intent);
+                }
+            });
+            return v;
+        }
+    }
+}
