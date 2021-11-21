58b256e40688a14b3166357e5c6c2adb21d95500
Chaemin-L <chaem12@kookmin.ac.kr>
2021-11-21 20:40:13 +0900

    style: Fix typo

--- app/src/main/java/com/example/mp2021_2_9/JoinActivity.java
+++ app/src/main/java/com/example/mp2021_2_9/JoinActivity.java
@@ -263,7 +263,7 @@ public class JoinActivity extends AppCompatActivity {
                     Map<String, Object> tempuser = new HashMap<>();
                     tempuser.put("id", strid);
                     tempuser.put("isManager", false);
-                    tempuser.put("password", strpw);
+                    tempuser.put("passward", strpw);
                     tempuser.put("phoneNum", strphone);
                     tempuser.put("userName", strname);
 
