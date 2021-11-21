566d239082e50eed4154a89ca3218ff5687a9036
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2021-11-21 19:10:14 +0900

    upload view pager

--- app/src/main/java/com/example/mp2021_2_9/PromoteMainFrag.java
+++ app/src/main/java/com/example/mp2021_2_9/PromoteMainFrag.java
@@ -4,17 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class PromoteMainFrag extends Fragment {
+    private ViewPager2 sliderViewPager;
+    private LinearLayout layoutIndicator;
+    private ViewGroup viewGroup;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View v = inflater.inflate(R.layout.promotion_main_screen, container, false);
-        return v;
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.promotion_main_screen, container, false);
+
+        setInit();
+
+        return viewGroup;
+    }
+
+    private void setInit(){
+        sliderViewPager = viewGroup.findViewById(R.id.sliderViewPager);
+        FragmentStateAdapter SetupPagerAdapter = new FragmentStateAdapter(getActivity()) {
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                return null;
+            }
+        };
     }
 }
