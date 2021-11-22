ad593b100036be6c0355a117d95a5f49d6c0fe22
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2021-11-22 10:23:11 +0900

    gridview주석처리 취소

--- app/src/main/java/com/example/mp2021_2_9/PromoteMainFrag.java
+++ app/src/main/java/com/example/mp2021_2_9/PromoteMainFrag.java
@@ -45,38 +45,38 @@ public class PromoteMainFrag extends Fragment {
         setInit();
 
 
-//        /* 상품 목록 부분 */
-//        boothInfo_lists = new ArrayList<>();    // 부스 정보 담을 arraylist
-//
-//        // DB 부분
-//        database = FirebaseDatabase.getInstance();                  // DB 루트 연결
-//        databaseReference = database.getReference("booth");    // booth 최상위 노드 연결
-//        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                // 파이어베이스의 데이터 받아오기
-//                boothInfo_lists.clear();    // 초기화
-//                for(DataSnapshot snapshot1 : snapshot.getChildren()){                   // 반복문 돌면서 데이터list 추출
-//                    BoothInfo_list tempList = snapshot1.getValue(BoothInfo_list.class); // 만들어둔 객체에 데이터 담기
-//                    boothInfo_lists.add(tempList);                                      // 리스트 저장
-//                }
-//                adapter.notifyDataSetChanged();     // 새로고침 - 반영
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                Log.e(TAG, String.valueOf(error.toException()));
-//            }
-//        });
-//
-//        // 리사이클러뷰에 GridLayoutManager 객체 지정
-//        recyclerView = viewGroup.findViewById(R.id.putRcv);
-//        recyclerView.setHasFixedSize(true);         // 리사이클러뷰 기존 성능 강화
-//        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));      // 그리드 레이아웃으로 설정
-//
-//        // 리사이클러뷰에 BoothAdapter 객체 지정
-//        adapter = new BoothAdapter(boothInfo_lists, getContext());
-//        recyclerView.setAdapter(adapter);
+        /* 상품 목록 부분 */
+        boothInfo_lists = new ArrayList<>();    // 부스 정보 담을 arraylist
+
+        // DB 부분
+        database = FirebaseDatabase.getInstance();                  // DB 루트 연결
+        databaseReference = database.getReference("booth");    // booth 최상위 노드 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // 파이어베이스의 데이터 받아오기
+                boothInfo_lists.clear();    // 초기화
+                for(DataSnapshot snapshot1 : snapshot.getChildren()){                   // 반복문 돌면서 데이터list 추출
+                    BoothInfo_list tempList = snapshot1.getValue(BoothInfo_list.class); // 만들어둔 객체에 데이터 담기
+                    boothInfo_lists.add(tempList);                                      // 리스트 저장
+                }
+                adapter.notifyDataSetChanged();     // 새로고침 - 반영
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, String.valueOf(error.toException()));
+            }
+        });
+
+        // 리사이클러뷰에 GridLayoutManager 객체 지정
+        recyclerView = viewGroup.findViewById(R.id.putRcv);
+        recyclerView.setHasFixedSize(true);         // 리사이클러뷰 기존 성능 강화
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));      // 그리드 레이아웃으로 설정
+
+        // 리사이클러뷰에 BoothAdapter 객체 지정
+        adapter = new BoothAdapter(boothInfo_lists, getContext());
+        recyclerView.setAdapter(adapter);
 
         return viewGroup;
     }
