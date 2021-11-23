c962de8d267c0e8cea4e89dd3cae15e39ce5ed08
solhee-hwang <66052043+solhee-hwang@users.noreply.github.com>
2021-11-23 17:35:51 +0900

    홍보 등록글

--- app/src/main/java/com/example/mp2021_2_9/AddPromotionActivity.java
+++ app/src/main/java/com/example/mp2021_2_9/AddPromotionActivity.java
@@ -5,36 +5,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddPromotionActivity extends Fragment {
+    int id;
     View view;
-    TextInputEditText boothname, boothlocation;
+    EditText boothname, boothlocation,boothtime;
     Button save;
+    DatabaseReference base = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference databaseReference = base.child("booth");
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_addpromotion, container, false);
         save = (Button)view.findViewById(R.id.savebutton);
-        boothname = (TextInputEditText) view.findViewById(R.id.booth_name);
-        boothlocation = (TextInputEditText) view.findViewById(R.id.booth_location);
+        boothname= (EditText) view.findViewById(R.id.booth_name);
+        boothlocation = (EditText) view.findViewById(R.id.booth_location);
+        boothtime = (EditText) view.findViewById(R.id.booth_time);
 
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addbooth(boothname.getText().toString(), boothlocation.getText().toString());
+                addbooth(boothlocation.getText().toString(), boothname.getText().toString(),boothtime.getText().toString(), "받을 아이디");
             }
         });
         return view;
     }
-    public void addbooth(String name, String location){
+    public void addbooth(String boothLocation, String boothName,String boothOpenTime, String userid){
+        AddPromotionData addPromotionData = new AddPromotionData(boothLocation, boothName,boothOpenTime,userid);
+        databaseReference.child("booth"+userid).setValue(addPromotionData);
 
     }
 
