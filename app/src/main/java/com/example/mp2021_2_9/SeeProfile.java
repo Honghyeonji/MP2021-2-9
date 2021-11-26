7e9e9f2c8069eb843fd6029a5fad8b0183f9b85e
wynter <gusdud0122@kookmin.ac.kr>
2021-11-27 04:48:15 +0900

    비번변경시 성공토스트

--- app/src/main/java/com/example/mp2021_2_9/SeeProfile.java
+++ app/src/main/java/com/example/mp2021_2_9/SeeProfile.java
@@ -148,6 +148,7 @@ public class SeeProfile extends Fragment{
                             Map<String, Object> map = new HashMap<String, Object>();
                             map.put("password", newPW.getText().toString());
                             myRef.child(loginID).updateChildren(map);
+                            Toast.makeText(getContext().getApplicationContext(), "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show();
                         }
                     }
 
