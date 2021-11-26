6a66717574718c8173bc267ffd38e3db3ae7ab09
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2021-11-26 13:16:20 +0900

    loadingPage complete

--- app/src/main/java/com/example/mp2021_2_9/MainActivity.java
+++ app/src/main/java/com/example/mp2021_2_9/MainActivity.java
@@ -1,33 +1,23 @@
 package com.example.mp2021_2_9;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
-
-import java.util.List;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity{
     private static final String TAG = "Main_Activity";
@@ -46,6 +36,8 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
 
         app_info.setKeyMap();
         app_info.setPageMap();
